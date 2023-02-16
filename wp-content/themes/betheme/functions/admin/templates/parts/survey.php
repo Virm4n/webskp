17163a3eae9ee1f916597e689563e3de30d80276
virm4n <virmansyah84@gmail.com>
2023-02-16 11:11:58 +0700

    masukinsmua

---
+++ wp-content/themes/betheme/functions/admin/templates/parts/survey.php
@@ -0,0 +1,12 @@
+<?php
+if( ! defined( 'ABSPATH' ) ){
+	exit; // Exit if accessed directly
+}
+?>
+
+<div class="mfn-survey">
+	<h3>Be needs your opinion to make the theme better for YOU.</h3>
+	<p>We’d appreciate it if you could take 5 minutes to answer a few questions about Be and its new Live Builder.</p>
+	<a class="button button-primary" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeIoabhhyoz4E5bXsxJWp3lUtQyKwAbYtGorkhONh0s5c9gYQ/viewform">Take survey</a>
+  <a class="close" href="#"><i class="icon-cancel-fine"></i></a>
+</div>
